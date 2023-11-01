2ede8c55e29e2206d2b136570d335e5e8cd05b67
sumanhong <suman0204@naver.com>
2023-11-01 10:03:03 +0900

    Initial Commit

---
+++ SeSACRxThreads/Resource.swift
@@ -0,0 +1,20 @@
+//
+//  Resource.swift
+//  SeSACRxThreads
+//
+//  Created by jack on 2023/11/01.
+//
+
+import UIKit
+
+enum Color {
+    // xcode 15
+//    static let black: UIColor = .textPoint
+//    static let white: UIColor = .viewPoints
+    
+    static let jack: UIColor = UIColor(named: "jackBorder")!
+    // xcode 14
+    static let black: UIColor = UIColor(named: "textPoint")!
+    static let white: UIColor = UIColor(named: "viewPoint")!
+    
+}
